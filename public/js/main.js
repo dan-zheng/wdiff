70d60ce2e09adddc3d3ae58718a7484637633cc0
Dan Zheng <zheng321@purdue.edu>
2017-01-13 18:57:27 -0500

    Add syntax highlighting, change API variable names

--- public/js/main.js
+++ public/js/main.js
@@ -33,7 +33,26 @@ $(document).ready(function() {
             }
         }
         html += '</div></div>';
-        html += '<pre id=\'jsonOutput\'>' + JSON.stringify(data.result, null, 4) + '</pre>';
+        html += '<pre id=\'jsonOutput\'>' + syntaxHighlight(JSON.stringify(data, null, 4)) + '</pre>';
         $('#results').html(html);
     });
 });
+
+function syntaxHighlight(json) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
